d5efaa59cc8d78eb2d8c9bac3cfae30a4ce200e7
graaff <graaff@75f390aa-dc20-0410-97be-87e14fff81df>
2002-12-25 16:22:34 +0000

    Input for generation has changed to checkbot

--- makeweb.sh
+++ makeweb.sh
@@ -4,7 +4,7 @@ echo -n "Cleaning out old files in website..."
 rm ${HOME}/projects/web-pages/degraaff.org/checkbot/*
 echo "done."
 echo -n "Creating updated manual..."
-pod2html --infile checkbot.pl --outfile checkbot.pl.html --htmlroot /checkbot/
+pod2html --infile checkbot --outfile checkbot.pl.html --htmlroot /checkbot/
 echo "done."
 echo -n "Copy files to website..."
 cp index.html style.css ChangeLog TODO checkbot.pl.html checkbot*.tar.gz ${HOME}/projects/web-pages/degraaff.org/checkbot/
